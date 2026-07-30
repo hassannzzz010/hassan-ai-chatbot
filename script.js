aa3adb2355d3a1962c1a6daebf350a06b0b185e8
hassannzzz010 <armaanmalik563457@gmail.com>
2026-07-31 02:43:39 +0500

    Refactor sendMessage function and update API URL

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ chatBox.scrollHeight;
 
     try {
 
-        const response = await fetch("http://127.0.0.1:5000/chat", {
+        const response = await fetch("https://hassannzzz010.pythonanywhere.com/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
